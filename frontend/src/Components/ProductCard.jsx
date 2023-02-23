d1b62245a639a1a584b027b0b3abdf1b3b892bff
aniketparmar29 <aniketparmar068@gmail.com>
2023-02-23 17:37:24 +0530

    aniket_day3

--- frontend/src/Components/ProductCard.jsx
+++ frontend/src/Components/ProductCard.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BsHeart } from 'react-icons/bs';
 import RatingBar from './RatingBar';
 
-function ProductCard({ key, product }) {
+function ProductCard({ product }) {
   const rating = (Math.random() * 5) + 1;
 
   return (
-    <div key={key} className="w-72 m-auto">
+    <div className="w-80">
       <div style={{ position: "relative" }}>
-        <img src={product.images} alt={product.title} />
+        <img src={product.images || product.images2 || product.images3 || product.images4 || product.images5 || product.images6 || product.images7 || product.images8 || `https://source.unsplash.com/random/?${product.category}` || "https://ii1.pepperfry.com/img/grey.gif"} alt={product.title} />
         <button className="bg-orange-500 text-white font-bold z-10 p-3 rounded absolute bottom-4 left-1/2 transform -translate-x-1/2 opacity-0 transition duration-300">
           Add To Cart
         </button>
@@ -23,25 +23,21 @@ function ProductCard({ key, product }) {
       <h3 className="text-left">{product.title}</h3>
       <p className="text-gray-500 text-left">{product.brand}</p>
       <div className="flex gap-x-2">
-        <h4 className="text-[rgb(255,124,70)] text- font-bold">
+        <h4 className="text-[rgb(255,124,70)] text-lg font-bold">
           ₹{product.priceoffer}
         </h4>
-        <h4 className="line-through">₹{product.mrp}</h4>
+        <h4 className="line-through text-lg">₹{product.mrp}</h4>
       </div>
       <h5 className="text-[rgb(104,175,93)] text-base text-left font-semibold">
         {product.discount}
       </h5>
       <style>{`
-        .w-72 > div:hover button {
+        .w-80:hover button {
           opacity: 1;
         }
       `}</style>
     </div>
   );
 }
-
-
-
-
 
 export default ProductCard;
--- frontend/src/Pages/ProductPage.jsx
+++ frontend/src/Pages/ProductPage.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import ProductList from './ProductList'
-
-function ProductPage() {
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../Redux/ProductReducer/action";
+function ProductPage({query}) {
+  const dispatch = useDispatch();
+  const products = useSelector((store) => store.ProductReducer.opp);
+  const filteredProducts = products.filter((product) => product.category === query);
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
   return (
     <div className='flex gap-x-4'>
       <div className="filter">filter</div>
-      <ProductList/>
+      <ProductList filteredProducts={filteredProducts}/>
     </div>
   )
 }
 
-export default ProductPage
+export default ProductPage;
+
+ProductPage.defaultProps = {
+  query :"sofasets"
+}
