ea9af953ae3bc5262a32f131c0b1c4eab4553964
vikram <vk14133m02059@gmail.com>
2023-02-23 10:16:19 +0530

    changes in store

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,14 +1,10 @@
-import {
-  legacy_createStore,
-  combineReducers,
-  applyMiddleware,
-} from "redux";
+import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import { reducer as AuthReducer } from "./AuthReducer/reducer";
 import { reducer as ProductReducer } from "./ProductReducer/reducer";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({ AuthReducer, ProductReducer });
 
-const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
 
 export { store };
