3f3b5e06aa3fd56c8fe1a1fe689e7d192bbfaa2f
aniketparmar29 <aniketparmar068@gmail.com>
2023-02-25 10:49:12 +0530

    day5

--- frontend/src/Pages/ProductList.jsx
+++ frontend/src/Pages/ProductList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ProductCard from "../Components/ProductCard";
 
-const ProductList = ({filteredProducts}) => {
+const ProductList = ({products,query}) => {
   return (
-    <div className="w-[75%] grid grid-cols-2 mt-8 sm:grid-cols-2 gap-y-4 lg:grid-cols-3">
-      {filteredProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className="w-[80%] grid grid-cols-2 mt-8 sm:grid-cols-2 gap-10 lg:grid-cols-3">
+      {products ? (
+        products.map((product) => (
+          <ProductCard key={product._id} product={product} query={query} />
+        ))
+      ) : (
+        <p>No products found</p>
+      )}
     </div>
   );
 };
