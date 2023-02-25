0f7f355043b8c86e1074778ebf0c1e92255b7e64
Vicky-ops <workmailvicky101@gmail.com>
2023-02-25 21:36:31 +0530

    adding redux setup for cart

--- frontend/src/Redux/AuthReducer/actionTypes.js
+++ frontend/src/Redux/AuthReducer/actionTypes.js
@@ -2,3 +2,6 @@
 export const GET_USERS_REQUEST = "GET_USERS_REQUEST";
 export const GET_USERS_SUCCESS = "GET_USERS_SUCCESS";
 export const GET_USERS_FAILURE = "GET_USERS_FAILURE";
+
+
+//vicky-ops 
---
+++ frontend/src/Redux/CartReducer/action.js
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from "react-redux";
+import * as actionTypes from "./actionTypes";
+
+
+export const addCardItem = (data) =>async(dispatch){
+    dispatch(_addCardItem);
+}
+
+
+export const _addCardItem = (data) =>{
+    return {
+        type:action.Types.ADD_CART_ITEM,
+        data
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+// products
+const Product = () =>{
+    const dispatch = useDispatch();
+    const {product:{product}} = useSelector((ob)=>obj);
+    useEffecct(()=>{
+        // get data from the API
+    })
+
+    addCardItem = (item)=>{
+        console.log(item)
+    }
+}
