77261ec58fdafaed629a7fb3cb1935fed1f33efc
Vicky-ops <workmailvicky101@gmail.com>
2023-02-26 16:30:05 +0530

    ---cart reducer updated to match the needs of the project

--- frontend/src/Home/components/NavbarSecond/Navbar.jsx
+++ frontend/src/Home/components/NavbarSecond/Navbar.jsx
@@ -71,18 +71,22 @@ const Navbar = () => {
             <label for="pincode" className="block mb-2 text-sm font-sm text-white dark:text-dark test-xs">Check</label>
          </div>
             
-         <div className="py-4 overflow-y-auto">
-      <ul className="space-y-2" style={{display:"flex"}}>
-         <li>
-            <button  className="flex active:text-black active:bg-white items-center p-2 m-0 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-white dark:hover:bg-gray-700">
-               {/* <svg aria-hidden="true" className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg> */}
-               <span className="ml-3">MY CART</span>
-            </button>
-         </li>
-         <li>
-            <button type="button" className="flex items-center active:text-white active:bg-black w-full text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+         <div className="overflow-y-auto">
+      <ul  style={{display:"flex"}} className="bg-black">
+        <li className='bg-black p-5'>
+        <button type="button" className="flex items-center active:text-black active:bg-white w-full text-base font-normal text-white transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
                   {/* <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg> */}
-                  <span className="flex-1 ml-3 text-left whitespace-nowrap">MY WISHLIST</span>
+                  <span className="flex-1 ml-3 text-left whitespace-nowrap">MY CART</span>
+                  {/* <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
+                  <button className="bg-orange text-white w-90%">PROCEED TO PAY SECURELY</button>
+            </button>
+           
+           
+         </li>
+         <li className='bg-black p-5'>
+            <button type="button"className="flex items-center active:text-black active:bg-white w-full text-base font-normal text-white transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                  {/* <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg> */}
+                  <span className="flex-2 ml-3 text-left whitespace-nowrap">MY WISHLIST</span>
                   {/* <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> */}
             </button>
             <ul id="dropdown-example" className="hidden py-2 space-y-2">
